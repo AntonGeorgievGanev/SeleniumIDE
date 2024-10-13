3e4b1e2211c3d16a73a480b6c98d56927cf7405a
Anton Ganev <122820049+AntonGeorgievGanev@users.noreply.github.com>
2024-10-13 12:07:39 +0300

    instance

--- SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
+++ SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36789f6551e48d03bd8e26846671f20b3c0175f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f77fadb46e6d92f995ef28923d07587b906a9f10")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
